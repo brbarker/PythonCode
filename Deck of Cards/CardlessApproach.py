fc8ee7c0a0278df43df54e3580484b4655f2a135
Brian Barker <bravo4586@gmail.com>
2023-05-14 19:39:12 -0400

    BB

--- Deck of Cards/CardlessApproach.py
+++ Deck of Cards/CardlessApproach.py
@@ -1,4 +1,16 @@
-newDeck=[]
-for i in range(1,53):
-    newDeck.append(i)
+Deck=[]
+topDeck=[]
+bottomDeck=[]
 
+for i in range(1,53):
+    Deck.append(i)
+
+
+
+def splitDeck():
+    for i in range(len(Deck)//2):
+        topDeck.append(Deck[i-1])
+
+splitDeck()
+print(Deck)
+print(topDeck)
