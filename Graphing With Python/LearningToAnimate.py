e79ecc43e3454ea2a3f451306ede0ce9dee63987
Brian Barker <bravo4586@gmail.com>
2023-06-09 11:01:28 -0400

    BB

--- Graphing With Python/LearningToAnimate.py
+++ Graphing With Python/LearningToAnimate.py
@@ -1,30 +1,27 @@
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import matplotlib.units as units
 import numpy as np
-from basic_units import radians
 
 # lists to store data
 xdata = []
 ydata = []
 
 # run through a range and randomly pick values to create sample datasets, then add them to lists for plotting
-for i in np.arange(-2*np.pi,2*np.pi,0.1):
+for i in np.arange(0,50,0.5):
     # add the values to the lists
     xdata.append(i)
-    ydata.append(np.sin(i))
+    ydata.append(np.random.choice(100))
+
+    
 
     # define the limits for the graph
-    plt.xlim(-2*np.pi,2*np.pi)
-    plt.ylim(-1.25,1.25)
-
-    ax = plt.axes
-    ax.plot(xunits=radians)
+    plt.xlim(-1,51)
+    plt.ylim(-1,101)
 
     # plot the graph with the axis shown and the grid lines shown
     plt.plot(xdata,ydata,color='red')
     plt.axis('on')
     plt.grid(True)
+
     # make a title for the graph
     plt.title('A Living Graph')
 
