4280a2447ce899e23cdafe39102074c3373f204d
Brian Barker <bravo4586@gmail.com>
2025-04-02 10:29:24 -0400

    BB

--- Graphing With Python/2DVectorField.py
+++ Graphing With Python/2DVectorField.py
@@ -1,2 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+x = np.arange(-20,20,1)
+
+y = np.arange(-20,20,1)
+
+u,v = np.meshgrid(x,y)
+
+field = (v,-u)
+
+fig, ax = plt.subplots()
+
+q = ax.quiver(x,y,*field)
+
+plt.show()
